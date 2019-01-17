c02331aeedda6872605263802488e5de22ae00a2
lescpsn <lescpsn@aliyun.com>
2019-01-17 18:28:07 +0800

    (*) 优化工程

--- gui/src/pages/index.js
+++ gui/src/pages/index.js
@@ -3,11 +3,19 @@ import styles from './index.css';
 export default function() {
   return (
     <div className={styles.normal}>
+      <div>
+          江苏省税务标杆自动化机器人
+      </div>
+
       <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
       </ul>
+
+      <div>
+          江苏省税务标杆自动化机器人
+      </div>
     </div>
   );
 }
