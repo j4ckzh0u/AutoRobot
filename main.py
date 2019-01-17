5c9b6bc5b99f85238bfadbd6206ba2291b6e0fb3
lescpsn <lescpsn@aliyun.com>
2019-01-17 10:27:01 +0800

    (*) 优化工程

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 ################################################################################
-# 
-# Author    : 乔帮主
+#
+# Author            : 乔帮主
 # Generate Date     : 2019/01/16
 #
 ################################################################################
-
 
 import autoit
 
@@ -14,16 +14,12 @@ def main():
     print("Enter main:")
 
     print("*************** t101:")
-    
 
-
-    autoit.run("notepad.exe") 
-    autoit.win_wait_active("[CLASS:Notepad]", 3) 
-    autoit.control_send("[CLASS:Notepad]", "Edit1", "hello world{!}") 
-    autoit.win_close("[CLASS:Notepad]") 
+    autoit.run("notepad.exe")
+    autoit.win_wait_active("[CLASS:Notepad]", 3)
+    autoit.control_send("[CLASS:Notepad]", "Edit1", "hello world{!}")
+    autoit.win_close("[CLASS:Notepad]")
     autoit.control_click("[Class:#32770]", "Button2")
-
-
 
     print("Level main:")
 
