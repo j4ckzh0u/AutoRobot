6aa43b684aee8f72d9402fc752bed264b6c42f46
lescpsn <lescpsn@aliyun.com>
2019-01-24 06:38:49 +0800

    (*) 优化工程

--- gui/src/pages/index.js
+++ gui/src/pages/index.js
@@ -12,15 +12,17 @@
 //   );
 // }
 
+
 import React, { PureComponent } from 'react'
 import Redirect from 'umi/redirect'
-// import { withI18n } from '@lingui/react'
+import { withI18n } from '@lingui/react'
 
-// @withI18n()
+@withI18n()
 class Index extends PureComponent {
   render() {
     const { i18n } = this.props
-    return <Redirect to={i18n.t`/dashboard`} />
+    // return <Redirect to={i18n.t`/dashboard`} />
+    return <Redirect to={`/dashboard`} />
   }
 }
 
