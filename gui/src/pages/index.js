619ef2427bd9de6c616660c8cdd97620dcd0ea9b
lescpsn <lescpsn@aliyun.com>
2019-01-23 23:10:27 +0800

    (*) 优化工程

--- gui/src/pages/index.js
+++ gui/src/pages/index.js
@@ -1,13 +1,27 @@
-import styles from './index.css';
+// import styles from './index.css';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+// export default function() {
+//   return (
+//     <div className={styles.normal}>
+//       <div className={styles.welcome} />
+//       <ul className={styles.list}>
+//         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+//         <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+import React, { PureComponent } from 'react'
+import Redirect from 'umi/redirect'
+// import { withI18n } from '@lingui/react'
+
+// @withI18n()
+class Index extends PureComponent {
+  render() {
+    const { i18n } = this.props
+    return <Redirect to={i18n.t`/dashboard`} />
+  }
 }
+
+export default Index
